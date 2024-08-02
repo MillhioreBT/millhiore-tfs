ff7953ef6468ff0dc9c8f61e024a5c718946921a
Sarah Wesker <sarahelizabetwesker@gmail.com>
2024-08-01 20:01:54 -0400

    vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
-import { defineConfig } from "astro/config"
+import { defineConfig } from "astro/config";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-	output: "server",
-	integrations: [],
-	experimental: {
-		serverIslands: true,
-	},
-})
+  output: "server",
+  integrations: [],
+  experimental: {
+    serverIslands: true
+  },
+  adapter: vercel()
+});
