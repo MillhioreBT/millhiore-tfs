1e90f7856854bb8409266dfad687e85c3999c6f8
Sarah Wesker <jehicris@hotmail.com>
2025-06-09 18:38:00 -0400

    Fix formatting and clean up drizzle.config.ts ✨🐾

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,10 @@
-import type { Config } from "drizzle-kit"
+import type { Config } from "drizzle-kit";
 
 export default {
-	schema: "./libs/schema.ts",
-	out: "./libs/migrations",
-	dialect: "sqlite",
-	driver: "turso",
-	dbCredentials: {
-		url: process.env.TURSO_DATABASE_URL!,
-		authToken: process.env.TURSO_AUTH_TOKEN,
-	},
-} satisfies Config
+  schema: "./libs/schema.ts",
+  out: "./libs/migrations",
+  dialect: "sqlite",
+  dbCredentials: {
+    url: process.env.TURSO_DATABASE_URL!,
+  },
+} satisfies Config;
